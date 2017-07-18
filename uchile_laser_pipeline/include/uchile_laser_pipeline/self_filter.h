3e9815985a57e4ee5a5b19b1d9a877984729244e
Matías Pavez <matias.pavez.b@gmail.com>
2017-07-18 01:37:58 -0400

    laser self filtering: dynamic reconfigure

--- uchile_laser_pipeline/include/uchile_laser_pipeline/self_filter.h
+++ uchile_laser_pipeline/include/uchile_laser_pipeline/self_filter.h
@@ -10,6 +10,11 @@
 #include <tf/transform_datatypes.h>
 #include <tf/transform_listener.h>
 
+#include <dynamic_reconfigure/server.h>
+#include <uchile_laser_pipeline/SelfFilterConfig.h>
+
+#include <visualization_msgs/Marker.h>
+#include <visualization_msgs/MarkerArray.h>
 
 namespace uchile_laser_pipeline
 {
@@ -24,20 +29,32 @@ class LaserScanSelfFilter : public filters::FilterBase<sensor_msgs::LaserScan>
       const sensor_msgs::LaserScan& input_scan,
       sensor_msgs::LaserScan& filtered_scan);
 
+    // dynamic reconfigure server
+    dynamic_reconfigure::Server<uchile_laser_pipeline::SelfFilterConfig> dyn_server;
+    dynamic_reconfigure::Server<uchile_laser_pipeline::SelfFilterConfig>::CallbackType dyn_f;
+    
   private:
 
     void getAngularLimits(float x, float y, float z, float r, float &theta_min, float &theta_max);
 
+    void dynamicReconfigureCallback(uchile_laser_pipeline::SelfFilterConfig &config, uint32_t level);
+
+    visualization_msgs::Marker genMarker(const std::string& target_frame, double radius, int marker_id);
+
     std::vector<std::string> _target_frames;
-    std::vector<double> _inflation_radius_list;
+    std::string _l_hand_frame, _r_hand_frame;
+    double _l_hand_radius, _r_hand_radius;
+    double _l_hand_inflated_radius, _r_hand_inflated_radius;
+    double _publish_markers;
 
     laser_geometry::LaserProjection projector_;
     
     // tf listener to transform scans into the box_frame
     tf::TransformListener tf_listener;
+
+    ros::Publisher marker_pub;
 };
 
 }
-
 
 #endif /* self_filter.h */
--- uchile_laser_pipeline/src/self_filter.cpp
+++ uchile_laser_pipeline/src/self_filter.cpp
@@ -4,48 +4,65 @@ uchile_laser_pipeline::LaserScanSelfFilter::LaserScanSelfFilter() {
 
 }
 
+void uchile_laser_pipeline::LaserScanSelfFilter::dynamicReconfigureCallback(uchile_laser_pipeline::SelfFilterConfig &config, uint32_t level) {
+    ROS_INFO("Reconfigure Request: {l,r} radius: %.2f, %.2f. Publish markers: %s", 
+            config.l_radius,
+            config.r_radius, 
+            config.publish_markers ? "True" : "False");
+    _publish_markers = config.publish_markers;
+    _l_hand_inflated_radius = std::max(_l_hand_radius, config.l_radius);
+    _r_hand_inflated_radius = std::max(_r_hand_radius, config.r_radius);
+}
+
 // TODO: wait for tfs a few seconds while configuring the pipeline
 bool uchile_laser_pipeline::LaserScanSelfFilter::configure() {
 
     bool succeeded = true;
-    if (!getParam("target_frames", _target_frames)) {
+    if (!getParam("l_hand_link", _l_hand_frame)) {
         ROS_ERROR(
-        "The 'target_frames' parameter is not set. It corresponds to a list of the frames to consider when"
-                " filtering the laser scan data for self collisions. e.g.: ['left_hand', 'right_hand'].");
+        "The 'l_hand_link' parameter is not set. Should be set to the tf frame used by the left hand.");
         succeeded = false;
     }
-
-    if (!getParam("inflation_radius_list", _inflation_radius_list)) {
-        ROS_ERROR("The 'inflation_radius_list' parameter is not set. It corresponds to a list inflation radius for each"
-                          "provided target frame. They must be constrained to [0, 0.5] meters."
-                          "The inflation is used to create target spheres to be removed from laser scans."
-                          " e.g.: ['0.3', 0.3].");
+    if (!getParam("r_hand_link", _r_hand_frame)) {
+        ROS_ERROR(
+        "The 'r_hand_link' parameter is not set. Should be set to the tf frame used by the right hand.");
         succeeded = false;
-    } else {
-        std::vector<double>::const_iterator it;
-        for(it = _inflation_radius_list.begin(); it != _inflation_radius_list.end(); ++it) {
-            if (*it < 0 || *it > 0.5) {
-                ROS_ERROR_STREAM("The 'inflation_radius_list' values must be in range [0, 0.5] meters. Provided: " << *it);
-                succeeded = false;
-            }
-        }
-
-        if (_inflation_radius_list.size() != _target_frames.size()) {
-            ROS_ERROR_STREAM("The 'inflation_radius_list' size ("
-                                     << _inflation_radius_list.size()
-                                     << ") does not match the 'target_frames' size ("
-                                     << _target_frames.size()
-                                     << ").");
-            succeeded = false;
-        }
     }
+    if (!getParam("l_hand_radius", _l_hand_radius)) {
+        ROS_ERROR(
+        "The 'l_hand_radius' parameter is not set. Should be set to the left hand radius.");
+        succeeded = false;
+    }
+    if (!getParam("r_hand_radius", _r_hand_radius)) {
+        ROS_ERROR(
+        "The 'r_hand_radius' parameter is not set. Should be set to the right hand radius.");
+        succeeded = false;
+    }
+    if (_l_hand_radius < 0 || _l_hand_radius > 0.5) {    
+        ROS_ERROR_STREAM("The 'l_hand_radius' value must be in range [0, 0.5] meters. Provided: " << _l_hand_radius);
+        succeeded = false;
+    }
+    if (_r_hand_radius < 0 || _r_hand_radius > 0.5) {    
+        ROS_ERROR_STREAM("The 'r_hand_radius' value must be in range [0, 0.5] meters. Provided: " << _r_hand_radius);
+        succeeded = false;
+    }
+    _target_frames.push_back(_l_hand_frame);
+    _target_frames.push_back(_r_hand_frame);
+    _l_hand_inflated_radius = _l_hand_radius;
+    _r_hand_inflated_radius = _r_hand_radius;
+    _publish_markers = false;
+
+    dyn_f = boost::bind(&uchile_laser_pipeline::LaserScanSelfFilter::dynamicReconfigureCallback, this, _1, _2);
+    dyn_server.setCallback(dyn_f);
+
+    ros::NodeHandle nh("~");
+    marker_pub = nh.advertise<visualization_msgs::MarkerArray>("markers", 0);
+
     ros::Duration(3).sleep();
     return succeeded;
 }
 
-bool uchile_laser_pipeline::LaserScanSelfFilter::update(
-        const sensor_msgs::LaserScan &input_scan,
-        sensor_msgs::LaserScan &output_scan) {
+bool uchile_laser_pipeline::LaserScanSelfFilter::update(const sensor_msgs::LaserScan &input_scan, sensor_msgs::LaserScan &output_scan) {
 
     ros::Time target_time = input_scan.header.stamp + ros::Duration().fromSec(input_scan.ranges.size()*input_scan.time_increment);
 
@@ -56,10 +73,16 @@ bool uchile_laser_pipeline::LaserScanSelfFilter::update(
     // copy
     output_scan = input_scan;
 
+    std::vector<double> _hand_radius;
+    _hand_radius.push_back(_l_hand_inflated_radius);
+    _hand_radius.push_back(_r_hand_inflated_radius);
+
+    int marker_id = 0;
     std::string error_msg;
     std::vector<std::string>::const_iterator it;
     std::vector<double>::const_iterator r_it;
-    for(it = _target_frames.begin(), r_it = _inflation_radius_list.begin(); it != _target_frames.end(); ++it, ++r_it) {
+    visualization_msgs::MarkerArray marker_msg;
+    for(it = _target_frames.begin(), r_it = _hand_radius.begin(); it != _target_frames.end(); ++it, ++r_it) {
 
         std::string target_frame = *it;
         float radius = (float)*r_it;
@@ -67,12 +90,12 @@ bool uchile_laser_pipeline::LaserScanSelfFilter::update(
         link_point.header.frame_id = target_frame;
 
         bool success = tf_listener.waitForTransform(
-                target_frame,
-                input_scan.header.frame_id,
-                target_time,
-                ros::Duration(1.0),
-                ros::Duration(0.01),
-                &error_msg
+            target_frame,
+            input_scan.header.frame_id,
+            target_time,
+            ros::Duration(1.0),
+            ros::Duration(0.01),
+            &error_msg
         );
         if(!success){
             ROS_WARN_STREAM_DELAYED_THROTTLE(2.0 , "Could not get transform for frame '"
@@ -120,15 +143,44 @@ bool uchile_laser_pipeline::LaserScanSelfFilter::update(
             }
         }
 
+        marker_msg.markers.push_back(genMarker(target_frame, radius, marker_id));
+        marker_id++;
     }
+    marker_pub.publish(marker_msg);
     return true;
+}
+
+visualization_msgs::Marker uchile_laser_pipeline::LaserScanSelfFilter::genMarker(const std::string& target_frame, double radius, int marker_id) {
+    visualization_msgs::Marker marker;
+    marker.header.frame_id = target_frame;
+    marker.header.stamp = ros::Time();
+    marker.lifetime = ros::Duration(0.5);
+    marker.ns = "inflated_hand";
+    marker.id = marker_id;
+    marker.type = visualization_msgs::Marker::SPHERE;
+    marker.action = visualization_msgs::Marker::ADD;
+    marker.pose.position.x = 0;
+    marker.pose.position.y = 0;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+    marker.scale.x = radius * 2.0;
+    marker.scale.y = radius * 2.0;
+    marker.scale.z = radius * 2.0;
+    marker.color.a = 0.5;
+    marker.color.r = 0.0;
+    marker.color.g = 1.0;
+    marker.color.b = 1.0;
+    return marker;
 }
 
 /**
  * The algorithm works as follows:
  * Each considered joint/frame on 'target_frames' is inflated and
  * considered as a sphere centered at (0,0,0) with radius given by
- * 'inflation_radius_list'.
+ * 'hand_radius'.
  *
  * Then, the sphere center is transformed into the laser scan frame,
  * centered at (x,y,z). If the sphere is too high or too low, then
